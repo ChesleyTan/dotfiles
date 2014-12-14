55ad2dd8bd54d849cb1552c46b2fdf3de12d7511
ChesleyTan <ChesleyTan97@gmail.com>
2014-12-13 23:10:00 -0500

    Add update submodule script

---
+++ submodule_update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git submodule foreach git checkout master
+git submodule foreach git pull
+git add $(git submodule | grep -o "\+[a-f0-9]*\s[^ ]*" | grep -o "[^ ]*$")
+git commit -m "Autoupdate submodules"
+
