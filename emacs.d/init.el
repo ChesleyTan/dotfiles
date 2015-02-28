686b502a108f6555efefdc5098f8a702fe620f68
Chesley Tan <chesleytan97@gmail.com>
2015-02-28 11:46:09 -0500

    Update package repo urls

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,8 +1,8 @@
 (when (>= emacs-major-version 24)
   (require 'package)
   (package-initialize)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
 )
 
 ;; check if gui running
