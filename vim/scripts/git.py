469c615aad4a0254321257d4e8a08588e3c6d18d
Chesley Tan <chesleytan97@gmail.com>
2018-05-05 14:17:05 -0400

    Quote directory paths; show remote info

--- vim/scripts/git.py
+++ vim/scripts/git.py
@@ -19,7 +19,7 @@ def finish():
 
 # Ported from ~/.vimrc, so we're not using python's regex libraries
 ########## Git Branch ##########
-output = subprocess.Popen("cd %s && git branch | grep '*' | grep -o '[^* ]*'" % PWD, shell=True, stdout=subprocess.PIPE).stdout.read().replace('"', '\\"').replace('\n','')
+output = subprocess.Popen("cd '%s' && git branch | grep '*' | grep -o '[^* ]*'" % PWD, shell=True, stdout=subprocess.PIPE).stdout.read().replace('"', '\\"').replace('\n','')
 if output == "" or "fatal" in output:
     sys.exit(0)
 else:
@@ -27,7 +27,7 @@ else:
     s += "[Git][" + branch_name + " " # Excludes trailing newline
 
 ########## Git Status ##########
-output = subprocess.Popen("cd %s && git status" % PWD, shell=True, stdout=subprocess.PIPE).stdout.read().replace('"', '\\"')
+output = subprocess.Popen("cd '%s' && git status" % PWD, shell=True, stdout=subprocess.PIPE).stdout.read().replace('"', '\\"')
 if output == "":
     pass
 else:
@@ -39,26 +39,26 @@ else:
         s += " \u0394"
 
 ########## Git Stash ##########
-output = subprocess.Popen("cd %s && git stash list | wc -l" % PWD, shell=True, stdout=subprocess.PIPE).stdout.read().replace('"', '\\"').replace('\n', '')
+output = subprocess.Popen("cd '%s' && git stash list | wc -l" % PWD, shell=True, stdout=subprocess.PIPE).stdout.read().replace('"', '\\"').replace('\n', '')
 if output != "0":
     s += " \u26c1 " + output
 
 s += "]"
 
 ########## Git Remote ##########
-#output = subprocess.Popen("cd %s && git remote" % PWD, shell=True, stdout=subprocess.PIPE).stdout.read().replace('"', '\\"')
-#remotes = output.split(' ')
-#if remotes == []:
-#    s += "no remotes"
-#else:
-#    remote_name = remotes[0].strip() # Get the name of the first remote
-#    output = subprocess.Popen("cd %s && git remote show %s | grep '%s'" % (PWD,
-#        remote_name, branch_name), shell=True, stdout=subprocess.PIPE).stdout.read().replace('"', '\\"')
-#    if output == "" or "fatal" in output:
-#        pass
-#    elif "local out of date" in output:
-#        s += "(!)Local repo out of date"
-#    else:
-#        pass # Local is up to date
+output = subprocess.Popen("cd '%s' && git remote" % PWD, shell=True, stdout=subprocess.PIPE).stdout.read().replace('"', '\\"')
+remotes = output.split(' ')
+if remotes == []:
+    s += "no remotes"
+else:
+    remote_name = remotes[0].strip() # Get the name of the first remote
+    output = subprocess.Popen("cd '%s' && git remote show '%s' | grep '%s'" % (PWD,
+        remote_name, branch_name), shell=True, stdout=subprocess.PIPE).stdout.read().replace('"', '\\"')
+    if output == "" or "fatal" in output:
+        pass
+    elif "local out of date" in output:
+        s += "(!)Local repo out of date"
+    else:
+        pass # Local is up to date
 
 finish()
