eb1178e1ccbf56d1f20e66ca1bc17e328728767d
Chesley Tan <chesleytan97@gmail.com>
2018-11-12 01:22:16 -0500

    Update setup script path for fzf

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PWD=$(pwd)
+SCRIPT_DIR=$(dirname "$0")
 # ANSI Escape Codes
 RED="\033[1;31m"
 GREEN="\033[1;32m"
@@ -15,9 +15,9 @@ if [[ $ans == "y" ]]; then
     elif [[ -a ~/.vim/ ]]; then
         printf "${RED}~/.vim/ already exists!${RESET}\n"
     else
-        ln -s $PWD/vimrc $HOME/.vimrc
+        ln -s $SCRIPT_DIR/vimrc $HOME/.vimrc
         printf "${GREEN}Linked ~/.vimrc${RESET}\n"
-        ln -s $PWD/vim $HOME/.vim
+        ln -s $SCRIPT_DIR/vim $HOME/.vim
         printf "${GREEN}Linked ~/.vim/${RESET}\n"
     fi
 fi
@@ -39,7 +39,7 @@ if [[ $ans == "y" ]]; then
     if [[ -a ~/.emacs.d/ ]]; then
         printf "${RED}~/.emacs.d/ already exists!${RESET}\n"
     else
-        ln -s $PWD/emacs.d $HOME/.emacs.d
+        ln -s $SCRIPT_DIR/emacs.d $HOME/.emacs.d
         printf "${GREEN}Linked ~/.emacs.d/${RESET}\n"
     fi
 fi
@@ -50,7 +50,7 @@ if [[ $ans == "y" ]]; then
     if [[ -a ~/.inputrc ]]; then
         printf "${RED}~/.inputrc already exists!${RESET}\n"
     else
-        ln -s $PWD/inputrc $HOME/.inputrc
+        ln -s $SCRIPT_DIR/inputrc $HOME/.inputrc
         printf "${GREEN}Linked ~/.inputrc${RESET}\n"
     fi
 fi
@@ -61,7 +61,7 @@ if [[ $ans == "y" ]]; then
     if [[ -a ~/.bashrc ]]; then
         printf "${RED}~/.bashrc already exists!${RESET}\n"
     else
-        ln -s $PWD/bashrc $HOME/.bashrc
+        ln -s $SCRIPT_DIR/bashrc $HOME/.bashrc
         printf "${GREEN}Linked ~/.bashrc${RESET}\n"
     fi
 fi
@@ -72,13 +72,13 @@ if [[ $ans == "y" ]]; then
     if [[ -a ~/.zshrc ]]; then
         printf "${RED}~/.zshrc already exists!${RESET}\n"
     else
-        ln -s $PWD/zshrc $HOME/.zshrc
+        ln -s $SCRIPT_DIR/zshrc $HOME/.zshrc
         printf "${GREEN}Linked ~/.zshrc${RESET}\n"
     fi
     if [[ -a ~/.zsh/ ]]; then
         printf "${RED}~/.zsh/ already exists!${RESET}\n"
     else
-        ln -s $PWD/include/zsh $HOME/.zsh
+        ln -s $SCRIPT_DIR/include/zsh $HOME/.zsh
         printf "${GREEN}Linked ~/.zsh/${RESET}\n"
         echo "Updating submodules...."
         git submodule init
@@ -92,13 +92,13 @@ if [[ $ans == "y" ]]; then
     if [[ -a ~/.tmux.conf ]]; then
         printf "${RED}~/.tmux.conf already exists!${RESET}\n"
     else
-        ln -s $PWD/tmux.conf $HOME/.tmux.conf
+        ln -s $SCRIPT_DIR/tmux.conf $HOME/.tmux.conf
         printf "${GREEN}Linked ~/.tmux.conf${RESET}\n"
     fi
     if [[ -a ~/.tmux/ ]]; then
         printf "${RED}~/.tmux/ already exists!${RESET}\n"
     else
-        ln -s $PWD/include/tmux $HOME/.tmux
+        ln -s $SCRIPT_DIR/include/tmux $HOME/.tmux
         printf "${GREEN}Linked ~/.tmux/${RESET}\n"
         echo "Updating submodules...."
         git submodule init
@@ -112,7 +112,7 @@ if [[ $ans == "y" ]]; then
     if [[ -a ~/.xmobarrc_bottom ]]; then
         printf "${RED}~/.xmobarrc_bottom already exists!${RESET}\n"
     else
-        ln -s $PWD/xmobarrc_bottom $HOME/.xmobarrc_bottom
+        ln -s $SCRIPT_DIR/xmobarrc_bottom $HOME/.xmobarrc_bottom
         printf "${GREEN}Linked ~/.xmobarrc_bottom${RESET}\n"
     fi
 fi
@@ -123,7 +123,7 @@ if [[ $ans == "y" ]]; then
     if [[ -a ~/.xmobarrc_top ]]; then
         printf "${RED}~/.xmobarrc_top already exists!${RESET}\n"
     else
-        ln -s $PWD/xmobarrc_top $HOME/.xmobarrc_top
+        ln -s $SCRIPT_DIR/xmobarrc_top $HOME/.xmobarrc_top
         printf "${GREEN}Linked ~/.xmobarrc_top${RESET}\n"
     fi
 fi
@@ -134,7 +134,7 @@ if [[ $ans == "y" ]]; then
     if [[ -a ~/.xmonad/ ]]; then
         printf "${RED}~/.xmonad/ already exists!${RESET}\n"
     else
-        ln -s $PWD/xmonad $HOME/.xmonad
+        ln -s $SCRIPT_DIR/xmonad $HOME/.xmonad
         printf "${GREEN}Linked ~/.xmonad/${RESET}\n"
     fi
 fi
@@ -144,7 +144,7 @@ read ans
 if [[ $ans == "y" ]]; then
     git submodule init
     git submodule update
-    $PWD/zsh/fzf/install
+    $SCRIPT_DIR/include/fzf/install
     printf "${GREEN}Installed fzf${RESET}\n"
 fi
 
